595ba6e2f427edb687ece758d81c99e1e1cb0b43
Dark <tomnic18@gmail.com>
2025-08-16 19:13:14 +0200

    added support for all screen resolution and all in game lenguages

--- genshin.py
+++ genshin.py
@@ -7,6 +7,7 @@ import random
 import pygetwindow as gw
 import json
 from pathlib import Path
+import math
 
 
 kb = Controller()
@@ -14,27 +15,61 @@ dialog_flag = False
 setting = Path(__file__).parent / "genshin.json"
 Welcome="Genshin Dialogue Skipper is ready to Start\n" \
         "Press F8 to start and stop the program"
-
+config = ''
+x = 0
+y = 0
 def start():
+    global config
+    global x,y
     if setting.is_file():
+        with open(setting, 'r') as f:
+            config = json.load(f)
+        x = config["X"]
+        y = config["Y"]
         print(Welcome)
         main()
     else:
-        config = {"lenguage": 0, "screensize":0}
-        config["screensize"] = input("Chose Screen Size\n 1:1920x1080\n")
-        config["lenguage"] = input("Chose Lenguage\n1:English\n2:Italian\n")
+        x,y = find_hide_button()
+        config = {"X": x, "Y": y}
         with open(setting, "w") as f:
             json.dump(config,f)
         print(Welcome)
         main()
-
-
 def main():
     thread2 = threading.Thread(target=dialog_skipper, args=())
     thread2.start()
 
     with Listener(on_press=on_press) as listener:
         listener.join()
+def find_hide_button():
+    lenguage = int(input("Chose Lenguage\n1:English and French\n2:Simplified and Traditional Chinese\n3:Korean\n4:Japanese\n5:Spanish,Portugese and Italian\n6:Indonesian\n7:Russian\n8:German\n9:Thai\n10:Turkish\n11:Vietnamese\n"))
+    if lenguage == 1: #English / French
+        rel_pos_x = 280
+    elif lenguage == 2: #Simplified Chinese / Traditional Chinese
+        rel_pos_x = 270
+    elif lenguage == 3:
+        rel_pos_x = 268 # Korean
+    elif lenguage == 4:
+        rel_pos_x = 342 # Japanese
+    elif lenguage == 5:
+        rel_pos_x = 364 # Spanish / Portuguese / Italian
+    elif lenguage == 6:
+        rel_pos_x = 335 # Indonesian
+    elif lenguage == 7:
+        rel_pos_x = 283 # Russian
+    elif lenguage == 8:
+        rel_pos_x = 377 # German
+    elif lenguage == 9:
+        rel_pos_x = 225 # Thai
+    elif lenguage == 10:
+        rel_pos_x = 337 # Turkish
+    elif lenguage == 11:
+        rel_pos_x = 325 # Vietnamese
+   
+    screen_width,screen_height = pyautogui.size()
+    x = math.floor((rel_pos_x * int(screen_width)) / 1920)
+    y = math.floor((48 * int(screen_height)) / 1080)
+    return x,y
 
 def on_press(key):
     global dialog_flag
@@ -48,23 +83,18 @@ def on_press(key):
     except AttributeError:
         pass
 def in_dialog():
-    global lenguage
-    if lenguage == 2:
-        if pyautogui.pixelMatchesColor(364,47,(59, 67, 84), tolerance=5):
-            #print("In Dialog Ita")
+    global x,y,config
+    if pyautogui.pixelMatchesColor(x,y,(59, 67, 84), tolerance=5):
+            print("Dialog")
             return True
-        #print("Not Dialog Ita")
-        return False
-    elif lenguage == 1:
-        if pyautogui.pixelMatchesColor(280,48,(59, 67, 84), tolerance=5):
-            #print("In Dialog Eng")
-            return True
-        #print("Not Dialog Eng")
-        return False
+    print("Not Dialog")
+    return False
 
 def game_focused():
-    if gw.getActiveWindowTitle() == "Genshin Impact":
+    window = gw.getActiveWindow()
+    if window and window.title == "Genshin Impact" or "原神" or "원신" or "原神":
         return True
+    print(window.title)
     return False
 
 def text_skip():
@@ -76,8 +106,6 @@ def dialog_option():
     kb.press('f')
     time.sleep(0.01)
     kb.release('f')
-
-
 
 def dialog_skipper():
     global dialog_flag
